541ea203140997ad14a1d34d71bf5a3c735d53dd
mayank6147 <1999mmmayank@gmail.com>
2025-02-13 13:47:13 +0530

    file submitted

---
+++ Employee.java
@@ -0,0 +1,19 @@
+package Expriments;
+
+public class Employee {
+	int id;
+    String name;
+    double salary;
+
+    Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Salary: " + salary;
+    }
+}
+
+
